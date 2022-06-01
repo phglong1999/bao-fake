dc3a88b35a5664dda86f3d3694c0d3cf409a195f
phglong1999 <56074254+phglong1999@users.noreply.github.com>
2022-06-01 10:06:15 +0700

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
         <a href="/">XE</a>
         <a href="/">THỜI TRANG TRẺ</a>
         <a href="/">BẠN ĐỌC</a>
-<a href="/">BẠN ĐỌC</a>
+
       </div>
     </div>
   );
